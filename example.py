f9d654e835be5eb23139f06b69a6064471de17b8
valevo <valevogelmann@gmail.com>
2025-04-20 17:24:08 +0200

    added flags

--- example.py
+++ example.py
@@ -15,7 +15,7 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating a signal (try 10)
-LED_BRIGHTNESS = 65      # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 60      # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -83,8 +83,17 @@ if __name__ == '__main__':
     # Process arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    parser.add_argument('--brightness', help='brightness (0=lowest, 255=brightest')
+    parser.add_argument('--numleds', help='number of LEDs to use (144 on the strip)')
+
     args = parser.parse_args()
 
+    if args.brightness:
+        LED_BRIGHTNESS = args.brightness
+
+    if args.numleds:
+        LED_COUNT = args.numleds
+    
     # Create NeoPixel object with appropriate configuration.
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
